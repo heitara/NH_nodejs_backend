3385bcb245219ff1fb8563408ff301184ec77e13
Emil Atanasov <heitara@gmail.com>
2024-10-29 18:28:35 +0200

    feat: add .gitignore

---
+++ test.js
@@ -0,0 +1,11 @@
+const greeting = "Hello, "
+let technologyName = "Node.JS"
+
+console.log(greeting + technologyName + "!")
+
+
+// 1. Install NodeJS on our computer
+// on Windows - manually 
+//             - using a package manager
+// on macOS and on Linux (Ubuntu)  
+// 2. Install a good text editor!!! (VSCode)
