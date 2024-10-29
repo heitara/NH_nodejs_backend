02a2959bc52ffe91962f5eca13e75568658986dc
Emil Atanasov <heitara@gmail.com>
2024-10-29 18:05:34 +0200

    feat: initial commit

---
+++ console.js
@@ -0,0 +1,7 @@
+//https://nodejs.org/docs/latest-v20.x/api/console.html#consolelogdata-args
+
+const count = 5;
+console.log('count: %d', count);
+// Prints: count: 5, to stdout
+console.log('count:', count);
+// Prints: count: 5, to stdout
