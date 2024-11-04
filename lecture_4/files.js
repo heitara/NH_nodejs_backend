74f688ba18e77e172e3e9ca6a8b036e3e5dea94f
Emil Atanasov <heitara@gmail.com>
2024-11-04 10:04:56 +0200

    feat: parse txt and convert it json

--- lecture_4/files.js
+++ lecture_4/files.js
@@ -1,9 +1,10 @@
-import fs from 'node:fs';
+// import fs from 'node:fs';
 
-// import fs from 'node:fs/promises'; 
+import fs from 'node:fs/promises'; 
+import { describe } from 'node:test';
 
 const options = {encoding: 'utf-8'};
-const filename = "new.app.js";
+// const filename = "new.app.js";
 // fs.readFile(filename, options, (error, data) => {
 //     if(error) {
 //         console.log("Error: ", error); 
@@ -27,12 +28,12 @@ const filename = "new.app.js";
 //     console.log(error);
 // }
 
-let d = new Date();
-const info = {
-    version: "1.0.0",
-    date: d.getTime(),
-    size: 1024
-};
+// let d = new Date();
+// const info = {
+//     version: "1.0.0",
+//     date: d.getTime(),
+//     size: 1024
+// };
 
 // try {
 //     const r = await fs.writeFile("output.json", JSON.stringify(info), options);
@@ -41,8 +42,35 @@ const info = {
 //     console.log(error);
 // }
 
-fs.writeFileSync("output.sync.json", JSON.stringify(info), options);
+// fs.writeFileSync("output.sync.json", JSON.stringify(info), options);
 
+const filename = "input.txt";
+const processData = (data) => {
+    let records = [];
 
-console.log("Finish!");
+   let rows = data.split('\n');
+
+   rows.forEach(row => {
+        const date = row.substring(0,10);
+        const [title, details] = row.split(";");
+
+        records.push({
+            date,
+            title: title.substring(11),
+            details
+        })
+   });
+
+    return records;
+}
+
+try {
+    const data = await fs.readFile(filename, options);
+    const jsonData = processData(data);
+    await fs.writeFile("output.from.input.json", JSON.stringify(jsonData), options);
+    // console.log(r);
+} catch (error) {
+    console.log(error);
+}
+// console.log("Finish!");
 
