273c69f76c8d1787563d611e16d3ec0166fed983
Emil Atanasov <heitara@gmail.com>
2024-11-12 20:34:07 +0200

    feat: create a dummy server

---
+++ processes/http/server.mjs
@@ -0,0 +1,12 @@
+import http from  "node:http";
+
+const server = http.createServer( (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello NodeJS!');
+});
+
+const port = 3000;
+
+server.listen(port, () => {
+    console.log("Server is running on http://localhost:" + port);
+});
