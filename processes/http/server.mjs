0b0bc18a880bbfc182e11e462d6d5a346b1f04dd
Emil Atanasov <heitara@gmail.com>
2024-11-14 20:41:37 +0200

    feat: print the request url

--- processes/http/server.mjs
+++ processes/http/server.mjs
@@ -1,8 +1,10 @@
 import http from  "node:http";
 
 const server = http.createServer( (req, res) => {
+    console.log("RQ.url: ", req.url ?? "no url");
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Hello NodeJS!');
+
 });
 
 const port = 3000;
@@ -10,3 +12,8 @@ const port = 3000;
 server.listen(port, () => {
     console.log("Server is running on http://localhost:" + port);
 });
+
+
+//Express
+//KOA
+//Fastly 
