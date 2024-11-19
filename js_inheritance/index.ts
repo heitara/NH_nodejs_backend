8aa97125bf2f4252df0c52df4573f593fe711332
Emil Atanasov <heitara@gmail.com>
2024-11-19 18:47:35 +0200

    feat: inheritance using prototype and Object.create; some samples with Object.assign

--- js_inheritance/index.ts
+++ js_inheritance/index.ts
@@ -64,7 +64,92 @@ console.log(Object.getPrototypeOf(inst) === Ctor.prototype); // true
 function Point(x, y) {
     this.x = x;
     this.y = y;
+    this.log = () => {
+        console.log(`[${this.x},${this.y}]`)
+    }
+}
+
+Point.prototype.toJSON = function () {
+    return JSON.stringify({x: this.x, y: this.y});
+    // return JSON.stringify({x: undefined, y: undefined});
+}
+
+Point.prototype.toJSONArrow = () => {
+    return JSON.stringify({x: this.x, y: this.y});
+    // this is not the actual instance
+    //return JSON.stringify({x: undefined, y: undefined});
 }
 
 const zeroPoint = new Point(0, 0);
 console.log("Point.prototype check! ", Object.getPrototypeOf(zeroPoint) === Point.prototype);
+
+zeroPoint.log();
+
+const one = new Point(1, 1);
+
+const json = zeroPoint.toJSON();
+console.log("Regular function:");
+console.log("JSON = ", json);
+console.log("JSON = ", one.toJSON());
+
+console.log("Arrow function:");
+console.log("JSON = ", zeroPoint.toJSONArrow());
+console.log("JSON = ", one.toJSONArrow());
+
+
+function Point3D() {
+    Point.call(this, 0, 0);
+    // this.x = 0;
+    // this.y = 0;
+    this.z = 0;
+}
+
+// very important to link Point3D with Point
+Point3D.prototype = Object.create(Point.prototype);
+Point3D.prototype.constructor = Point3D;
+
+const p3d = new Point3D();
+
+console.log(p3d);
+
+const movablePoint3d = Object.create(p3d, {
+    move: {
+        value(x, y, z) {
+            this.x += x;
+            this.y += y;
+            this.z += z;
+        }
+    }
+});
+
+movablePoint3d.move(1, 1, 1);
+console.log(movablePoint3d);
+
+
+console.log(movablePoint3d instanceof Point3D);
+console.log(movablePoint3d instanceof Point);
+
+
+// const obj = {};
+// const obj2 = {};
+// console.log(Object.is(obj, obj2));
+// console.log(obj === obj2);
+// console.log(obj == obj2);
+
+
+
+const target = {x: 1, y: 2};
+const source = {y: 3, z: 4};
+
+const merged = Object.assign(target, source);
+
+console.log(merged);
+console.log(target);
+console.log(Object.is(merged, target));
+
+
+const target1 = {x: 1, y: 2};
+const source1 = {y: 3, z: 4};
+const source2 = {y1: 3, z: 10};
+
+console.log(Object.assign(target1, source1, source2));
