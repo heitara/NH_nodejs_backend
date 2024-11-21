6387772e111f864345c41baf4fbc29cf643d03bb
Emil Atanasov <heitara@gmail.com>
2024-11-21 18:43:53 +0200

    feat: some samples with asert.deepEqual

---
+++ testing/deepAssert.js
@@ -0,0 +1,38 @@
+const assert = require('node:assert');
+
+const obj1 = {
+  a: {
+    b: 1,
+  },
+};
+const obj2 = {
+  a: {
+    b: '1', // fails with b: 2
+  },
+};
+const obj3 = {
+  a: {
+    b: 1,
+  },
+};
+// const obj4 = { __proto__: obj1 };
+
+assert.deepEqual(obj1, obj1, 'Objects are not equal!');
+
+
+// Values of b are different:
+assert.deepEqual(obj1, obj2);
+
+//assert.deepStrictEqual(obj1, obj2); // these are different
+
+assert.deepEqual(obj1, obj3);
+
+// assert.deepEqual(obj1, obj4);
+// AssertionError: { a: { b: 1 } } deepEqual {}
+
+assert.doesNotThrow(
+    () => {
+        throw new TypeError('Wrong value');
+    },
+    SyntaxError,
+);
