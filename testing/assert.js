a818a584a51bfe76215d8d62fa81e117da581ddb
Emil Atanasov <heitara@gmail.com>
2024-11-21 18:09:06 +0200

    feat: assert.srtictEqual example

--- testing/assert.js
+++ testing/assert.js
@@ -1,6 +1,11 @@
 function sum(a, b){
     assert(!isNaN(a), `${a} is not a number`);
     assert(!isNaN(b), `${b} is not a number`);
+
+    if (a == 0) { return b; }
+    // there is a bug in here
+    if (b == 0) { return b; }
+
     return a + b;
 }
 
@@ -8,7 +13,11 @@ const assert = require('node:assert');
 
 assert(1 + 1 === 2, 'Sum is correct!');
 
-assert(1 + 1 === 3, 'Sum is incorrect!');
+// assert(1 + 1 === 3, 'Sum is incorrect!');
+
+sum("hello", "Node!");
 
 console.log("End!");
 
+
+assert.strictEqual(sum(2, 2), 4, "sum() is not working as expected!");
